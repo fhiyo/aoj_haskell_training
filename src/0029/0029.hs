c222eac0a602e650b24ef9916b9208b549eb34f9
fhiyo <fhhyodo@gmail.com>
2017-01-03 01:36:04 +0900

    Add 0029

---
+++ src/0029/0029.hs
@@ -0,0 +1,18 @@
+import Data.List
+import Data.Ord
+import Control.Arrow
+
+main :: IO ()
+main = getContents >>= putStrLn . (\(f, s) -> f ++ " " ++ s) . extractFreqAndLongestWord . map (length &&& head) . makeFreqList [[]] . sort . words
+
+extractFreqAndLongestWord :: [(Int, String)] -> (String, String)
+extractFreqAndLongestWord list = (snd (minimumBy (flip compare) list), snd . head $ sortBy (flip . comparing $ length . snd) list)
+
+makeFreqList :: [[String]] -> [String] -> [[String]]
+makeFreqList nested_list [] = nested_list
+makeFreqList [[]] v_list@(v:vs) = makeFreqList [[v]] vs
+makeFreqList [] v_list@(v:vs) = makeFreqList [[v]] vs
+makeFreqList nested_list@(x:xs) v_list@(v:vs) =
+  if head x == v
+  then makeFreqList ((x ++ [v]) : xs) vs
+  else x : makeFreqList xs v_list

def8c680ddf094771391177b25b82aac60049f27
fhiyo <fhhyodo@gmail.com>
2017-01-03 20:12:02 +0900

    Add annotation

--- src/0008/0008.hs
+++ src/0008/0008.hs
@@ -1,3 +1,6 @@
+-- XXX: I referred to other people's answers.
+-- From: http://judge.u-aizu.ac.jp/onlinejudge/review.jsp?rid=1500617#1
+
 main :: IO ()
 main = getContents >>= mapM_ (print . length . solve . read) . lines
 

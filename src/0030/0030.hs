b01724b144cfdf1ca3aa9fd9b863dc8b73461a43
fhiyo <fhhyodo@gmail.com>
2017-01-03 20:13:41 +0900

    Add 0030

---
+++ src/0030/0030.hs
@@ -0,0 +1,4 @@
+import Control.Monad
+
+main :: IO ()
+main = getContents >>= mapM_ (print . length . (\(lists, s) -> filter (\l -> sum l == s) lists) . (\[n, s] -> (filter (\l -> length l == n) $ filterM (\_ -> [True, False]) [0..9], s))) . takeWhile (\[n, s] -> [n, s] /= [0, 0]) . map (map (read :: String -> Int) . words) . lines
